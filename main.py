4db9ec1a06274f499169f5c864c6b7a7ca5177a7
kolya <kolanakolana1@gmail.com>
2021-12-16 19:14:34 +0300

    ver beta 1

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import psycopg2
 
 bot = telebot.TeleBot('5050419657:AAEVrwvvdC1UCF-1eK0hU7ftKVoCeGsBjKQ')
 parity = -1
+week = 0
+
 
 all_buttons = ["Понедельник", 'Вториник', "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье",
                "Расписание на текущуюю неделю", "Расписание на следующую неделю", '/help']
+
+tabels_names = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
 
 conn = psycopg2.connect(database="timetabel_db",
                         user="postgres",
@@ -34,6 +38,11 @@ def iNeedHelp(message):
                      reply_markup=keyboard)
 
 
+@bot.message_handler(commands=['week'])
+def tooday_week(message):
+    bot.send_message(message.chat.id, f'Текущая неделя: {week}')
+
+
 @bot.message_handler(content_types=['text'])
 def answer(message):
     if message.text in all_buttons:
@@ -41,38 +50,49 @@ def answer(message):
             calc_parity()
         index = all_buttons.index(message.text)
         if index <= 5:
-            printTimetabel(message)
+            printTimetabel(message, index)
         if index == 6:
-            bot.send_message(message.chat.id,'Этот день создан для отдыха')
+            bot.send_message(message.chat.id, 'Этот день создан для отдыха')
         if index == 7 or index == 8:
-            printTimetabel(message, True, int((index + 1) % 2))
+            printTimetabel(message, index, True, int((index + 1) % 2))
 
 
-def printTimetabel(message, all_days=False, reverse_parity=0):
-    if reverse_parity == 1:
+def printTimetabel(message, index, all_days=False, reverse_parity=0):
+    if reverse_parity == 0:
         par = True if parity == 0 else False
     else:
         par = False if parity == 0 else True
-    print('четность ' + str(par))
-
-    cursor.execute("SELECT * FROM timetabel")
-    records = list(cursor.fetchall())
+    # print('четность ' + str(par))
 
     line = ''
     if all_days == False:
-        for row in records:
-            if str(row[1]) == str(par) and row[2] == message.text:
-                line += row[3] + ' ' + row[4] + ' ' + row[5] + ' ' + row[6] + '\n'
+        cursor.execute(f"SELECT * FROM {tabels_names[index]}")
+        records = list(cursor.fetchall())
+
+        line += '-----=====' + all_buttons[index] + '=====-----' + '\n'
+        for i in range(len(records)):
+            if str(records[i][2]) == str(par) and records[i][4] != 'Нет пары':
+                audience = ', Аудитория: ' + records[i][6] if records[i][6] != '-' else ', Дистанционно'
+                line += records[i][4] + audience + ', ' + records[i][5] + ', ' + records[i][7] + '\n'
     else:
-        for i in range(7):
-            for row in records:
-                if str(row[1]) == str(par) and row[2] == all_buttons[i]:
-                    line += row[3] + ' ' + row[4] + ' ' + row[5] + ' ' + row[6] + '\n'
+        for i in range(6):
+            cursor.execute(f"SELECT * FROM {tabels_names[i]}")
+            records = list(cursor.fetchall())
+            line += '-----=====' + all_buttons[i] + '=====-----' + '\n'
+            for i in range(len(records)):
+                if str(records[i][2]) == str(par) and records[i][4] != 'Нет пары':
+                    audience = ', Аудитория: ' + records[i][6] if records[i][6] != '-' else ', Дистанционно'
+                    line += records[i][4] + ', ' + records[i][5] + audience + ', ' + records[i][7] + '\n'
     bot.send_message(message.chat.id, line)
 
 
+@bot.message_handler()
+def wrong(message):
+    bot.send_message(message.chat.id, 'Неверная команда!')
+
+
 def calc_parity():
-    global parity
+    global parity, week
     start_time = int(time.time())
     today = int(time.time())
 
@@ -82,7 +102,8 @@ def calc_parity():
     start_time = calc_monday(start_time)
     today = calc_monday(today)
     weeks_count = (today - start_time) / 604800 + 1
-    print(weeks_count)
+    # print(weeks_count)
+    week = int(weeks_count)
     parity = int(weeks_count % 2)
 
 
@@ -100,6 +121,7 @@ def add_keybord():
 
 
 if __name__ == '__main__':
+    calc_parity()
     bot.polling(none_stop=True)
     '''while True:
         try:
