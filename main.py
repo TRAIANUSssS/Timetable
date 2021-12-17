02fea2d94a157d057bcf15ac6aaafa747467fdc0
kolya <kolanakolana1@gmail.com>
2021-12-17 14:41:52 +0300

    ver beta 1.1

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ bot = telebot.TeleBot('5050419657:AAEVrwvvdC1UCF-1eK0hU7ftKVoCeGsBjKQ')
 parity = -1
 week = 0
 
-
 all_buttons = ["Понедельник", 'Вториник', "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье",
                "Расписание на текущуюю неделю", "Расписание на следующую неделю", '/help']
 
@@ -19,6 +18,10 @@ conn = psycopg2.connect(database="timetabel_db",
                         host="localhost",
                         port="5432")
 cursor = conn.cursor()
+
+@bot.message_handler()
+def wrong(message: types.Message):
+    bot.send_message(message.chat.id, 'Неверная команда!')
 
 
 @bot.message_handler(commands=['start'])
@@ -57,6 +60,11 @@ def answer(message):
             printTimetabel(message, index, True, int((index + 1) % 2))
 
 
+@bot.message_handler()
+def wrong(message: types.Message):
+    bot.send_message(message.chat.id, 'Неверная команда!')
+
+
 def printTimetabel(message, index, all_days=False, reverse_parity=0):
     if reverse_parity == 0:
         par = True if parity == 0 else False
@@ -64,31 +72,37 @@ def printTimetabel(message, index, all_days=False, reverse_parity=0):
         par = False if parity == 0 else True
     # print('четность ' + str(par))
 
-    line = ''
     if all_days == False:
         cursor.execute(f"SELECT * FROM {tabels_names[index]}")
         records = list(cursor.fetchall())
 
-        line += '-----=====' + all_buttons[index] + '=====-----' + '\n'
-        for i in range(len(records)):
-            if str(records[i][2]) == str(par) and records[i][4] != 'Нет пары':
-                audience = ', Аудитория: ' + records[i][6] if records[i][6] != '-' else ', Дистанционно'
-                line += records[i][4] + audience + ', ' + records[i][5] + ', ' + records[i][7] + '\n'
+        line = get_line_with_timetable(records, par, index)
     else:
         for i in range(6):
             cursor.execute(f"SELECT * FROM {tabels_names[i]}")
             records = list(cursor.fetchall())
-            line += '-----=====' + all_buttons[i] + '=====-----' + '\n'
-            for i in range(len(records)):
-                if str(records[i][2]) == str(par) and records[i][4] != 'Нет пары':
-                    audience = ', Аудитория: ' + records[i][6] if records[i][6] != '-' else ', Дистанционно'
-                    line += records[i][4] + ', ' + records[i][5] + audience + ', ' + records[i][7] + '\n'
+            line = ' '
+            line += get_line_with_timetable(records, par, index)
     bot.send_message(message.chat.id, line)
 
 
 @bot.message_handler()
-def wrong(message):
+def wrong(message: types.Message):
     bot.send_message(message.chat.id, 'Неверная команда!')
+
+
+def get_line_with_timetable(records, par, index):
+    line = ' '
+    iteartion = 0
+    line += '-----=====' + all_buttons[index] + '=====-----' + '\n'
+    for i in range(len(records)):
+        if str(records[i][2]) == str(par) and records[i][4] != 'Нет пары':
+            audience = ', Аудитория: ' + records[i][6] if records[i][6] != '-' else ', Дистанционно'
+            line += records[i][4] + audience + ', ' + records[i][5] + ', ' + records[i][7] + '\n'
+            iteartion += 1
+    if iteartion == 0:
+        line += "День отдыха" + '\n'
+    return line
 
 
 def calc_parity():
@@ -96,10 +110,7 @@ def calc_parity():
     start_time = int(time.time())
     today = int(time.time())
 
-    while time.strftime('%Y', time.gmtime(start_time)) != '2021' or time.strftime('%B', time.gmtime(
-            start_time)) != 'September' or time.strftime('%d', time.gmtime(start_time)) != '01':
-        start_time -= 86400
-    start_time = calc_monday(start_time)
+    start_time = calc_monday(start_time, True)
     today = calc_monday(today)
     weeks_count = (today - start_time) / 604800 + 1
     # print(weeks_count)
@@ -107,9 +118,14 @@ def calc_parity():
     parity = int(weeks_count % 2)
 
 
-def calc_monday(data):
-    while time.strftime('%A', time.gmtime(data)) != 'Monday':
-        data -= 86400
+def calc_monday(data, sep=False):
+    if sep == False:
+        while time.strftime('%A', time.gmtime(data)) != 'Monday':
+            data -= 86400
+    else:
+        while time.strftime('%Y', time.gmtime(data)) != '2021' or time.strftime('%B', time.gmtime(
+                data)) != 'September' or time.strftime('%d', time.gmtime(data)) != '01':
+            data -= 86400
     return data
 
 
